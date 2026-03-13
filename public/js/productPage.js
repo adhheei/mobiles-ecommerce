81e4b2811a1cc440f753639240f95d3da56abb3c
adithyaadhiii <adithyamv06@gmail.com>
2026-03-13 16:54:17 +0530

    fix: update wishlist api path in product page

--- public/js/productPage.js
+++ public/js/productPage.js
@@ -446,7 +446,7 @@ async function loadUserWishlist() {
   }
 
   try {
-    const res = await fetch("/api/wishlist", {
+    const res = await fetch("/api/user/wishlist", {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
