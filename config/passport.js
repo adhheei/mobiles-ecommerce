6ec1c1b9b5364be540912f2d8bfefaeade7ff633
adithyaadhiii <adithyamv06@gmail.com>
2026-03-14 13:31:39 +0530

    fix:google oauth for production domain

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/api/auth/google/callback",
+  callbackURL: "https://jinsamobiles.online/api/auth/google/callback",
   proxy: true
 },
   async (accessToken, refreshToken, profile, done) => {
